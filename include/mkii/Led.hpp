fccc85c5eb3692ddc88fca9472eda6cb4d871de4
Emilio Rojas <emilio93@gmail.com>
2019-04-07 18:37:51 -0600

    Update Led header and implementation

--- include/mkii/Led.hpp
+++ include/mkii/Led.hpp
@@ -6,65 +6,78 @@
 #include "mkii/Led.hpp"
 #include "peripheral/Timer32.hpp"
 
-namespace mkii{
+namespace mkii {
+/**
+ * @brief The top module acts as a container which manages and communicates
+ * all the components.
+ */
+class Led {
+ public:
   /**
-   * @brief The top module acts as a container which manages and communicates
-   * all the components.
+   * Class constructor sets port and pin of led.
    */
-  class Led {
-  public:
+  Led(uint32_t i_u32Port, uint32_t i_u32Pin);
 
+  /**
+   * Set led output.
+   */
+  void SetState(bool i_bState);
 
-    /**
-     * Class constructor.
-     */
-    Led();
+  /**
+   * Obtain current led output.
+   */
+  bool GetState();
 
-    /**
-     * Blink led i_u32BlinkCount times for a timer count i_u32TimerCount.
-     * This will cause a timer to count up to i_u32TimerCount for
-     * 2*i_u32BlinkCount times, toggling the led's output each time.
-     * @param i_u32BlinkCount Quantity of times the led blinks.
-     * @param i_u32TimerCount Timer Counter.
-     */
-    void Blink(uint32_t i_u32BlinkCount, uint32_t i_u32TimerCount);
+  /**
+   * Toggle output of led. If out value is 0, sets it to 1, else sets it to 0.
+   */
+  void Toggle();
 
-    /**
-     * Obtain Led object.
-     */
-    mkii::Led* GetMkiiLed(void);
+  /**
+   * Blink led i_u32BlinkCount times for a timer count i_u32TimerCount.
+   * This will cause a timer to count up to i_u32TimerCount for
+   * 2*i_u32BlinkCount times, toggling the led's output each time.
+   * @param i_u32BlinkCount Quantity of times the led blinks.
+   * @param i_u32TimerCount Timer Counter.
+   */
+  void Blink(uint32_t i_u32BlinkCount, uint32_t i_u32TimerCount);
 
-  private:
-    /**
-     * Initial blink count.
-     */
-    static uint32_t m_u32BlinkCount;
+ private:
+  /**
+   * Initial blink count.
+   */
+  static uint32_t m_u32BlinkCount;
 
-    /**
-     * Initial blink timer count.
-     */
-    static uint32_t m_u32TimerCount;
+  /**
+   * Initial blink timer count.
+   */
+  static uint32_t m_u32TimerCount;
 
-    /**
-     * Indicates if a blinking is ongoing.
-     */
-    static bool m_bIsBlinking;
+  /**
+   * Indicates if a blinking is ongoing.
+   */
+  static bool m_bIsBlinking;
 
-    /**
-     * Additional bit on m_u32BlinkCounter enables 2 toggles per count.
-     */
-    static bool m_bBlinkCountHasToggle;
+  /**
+   * Additional bit on m_u32BlinkCounter enables 2 toggles per count.
+   */
+  static bool m_bBlinkCountHasToggle;
 
-    /**
-     * Mkii led instance.
-     */
-    mkii::Led* m_mkiiLed;
+  /**
+   * Led port.
+   */
+  uint32_t m_u32Port;
 
-    /**
-     * The blink interrupt handler.
-     */
-    static void BlinkInterruptHandler(void);
-  };
-}
+  /**
+   * Led pin.
+   */
+  uint32_t m_u32Pin;
+
+  /**
+   * The blink interrupt handler.
+   */
+  static void BlinkInterruptHandler(void);
+};
+}  // namespace mkii
 
 #endif /* LUNAV_LED_H */
--- src/mkii/Led.cpp
+++ src/mkii/Led.cpp
@@ -1,22 +1,36 @@
+#include <string>
+
 #include "Top.hpp"
 #include "mkii/Led.hpp"
+#include "peripheral/Timer32.hpp"
 
-using namespace mkii{
-bool Led::m_bIsBlinking = false;
-uint32_t Led::m_u32BlinkCount = 0;
-uint32_t Led::m_u32TimerCount = 0;
-bool Led::m_bBlinkCountHasToggle = false;
+bool mkii::Led::m_bIsBlinking = false;
+uint32_t mkii::Led::m_u32BlinkCount = 0;
+uint32_t mkii::Led::m_u32TimerCount = 0;
+bool mkii::Led::m_bBlinkCountHasToggle = false;
 
-Led::Led() {
-  this->m_mkiiLed = new mkii::Led();
+mkii::Led::Led(uint32_t i_u32Port, uint32_t i_u32Pin) {
+  this->m_u32Port = i_u32Port;
+  this->m_u32Pin = i_u32Pin;
 }
 
-mkii::Led* Led::GetMkiiLed(void) {
-  return this->m_mkiiLed;
+void mkii::Led::SetState(bool i_bState) {
+  // TODO [emilio]
+  ;
 }
 
-void Led::Blink(uint32_t i_u32BlinkCount, uint32_t i_u32TimerCount) {
-  if (Led::m_bIsBlinking) {
+bool mkii::Led::GetState(void) {
+  // TODO [emilio]
+  return true;
+}
+
+void mkii::Led::Toggle() {
+  // TODO [emilio]
+  this->SetState(!this->GetState());
+}
+
+void mkii::Led::Blink(uint32_t i_u32BlinkCount, uint32_t i_u32TimerCount) {
+  if (mkii::Led::m_bIsBlinking) {
     // Another blink is in course.
     // TODO: define behaviour.
     // Current behaviour is to not execute blink.
@@ -27,52 +41,49 @@ void Led::Blink(uint32_t i_u32BlinkCount, uint32_t i_u32TimerCount) {
     return;
   }
 
-  // Set interrupt handler
-  void (*l_pIRQHandler)() = this->BlinkInterruptHandler;
-  Top::GetTop()->GetTimer32()->RegisterInterruption(true, l_pIRQHandler);
-
   // Set blinking initial settings
-  Led::m_u32BlinkCount = i_u32BlinkCount;
-  Led::m_bBlinkCountHasToggle = false;
-  Led::m_u32TimerCount = i_u32TimerCount;
-  Led::m_bIsBlinking = true;
+  mkii::Led::m_bIsBlinking = true;
+  mkii::Led::m_bBlinkCountHasToggle = false;
+  mkii::Led::m_u32BlinkCount = i_u32BlinkCount;
+  mkii::Led::m_u32TimerCount = i_u32TimerCount;
+
+  // Set interrupt handler
+  Top::GetTop()->GetTimer32()->RegisterInterrupt(true, this->BlinkInterruptHandler);
 
   // First call as soon as posible
-  Led::BlinkInterruptHandler();
+  mkii::Led::BlinkInterruptHandler();
 }
 
-void Led::BlinkInterruptHandler(void) {
-  if (Led::m_bIsBlinking) {
+void mkii::Led::BlinkInterruptHandler(void) {
+  if (mkii::Led::m_bIsBlinking) {
     // Always toggle led and clear interrupt flag
-    Top::GetTop()->GetLed()->GetMkiiLed()->Toggle();
-    Top::GetTop()->GetTimer32()->ClearInterruptionFlag();
+    Top::GetTop()->GetLed()->Toggle();
+    Top::GetTop()->GetTimer32()->ClearInterruptFlag();
 
-    if (Led::m_u32BlinkCount == 0 && Led::m_bBlinkCountHasToggle) {
+    if (mkii::Led::m_u32BlinkCount == 0 && mkii::Led::m_bBlinkCountHasToggle) {
       // Stop condition: counter has reached 0 and both toggles for last blink
       // have happened.
       // No further interrupt is done and handler is unregistered. Blinking
       // state is turned off.
-      Top::GetTop()->GetTimer32()->EnableInterruption(false);
-      Top::GetTop()->GetTimer32()->RegisterInterruption(false);
-      Led::m_bIsBlinking = false;
+      Top::GetTop()->GetTimer32()->EnableInterrupt(false);
+      Top::GetTop()->GetTimer32()->RegisterInterrupt(false);
+      mkii::Led::m_bIsBlinking = false;
       return;
     } else {
       // Regular operation: do both toggles for each count and reset timer and
       // interrupt.
-      if (Led::m_bBlinkCountHasToggle) {
-        Led::m_u32BlinkCount = Led::m_u32BlinkCount - 1;
+      if (mkii::Led::m_bBlinkCountHasToggle) {
+        mkii::Led::m_u32BlinkCount = mkii::Led::m_u32BlinkCount - 1;
       }
-      Led::m_bBlinkCountHasToggle = !Led::m_bBlinkCountHasToggle;
+      mkii::Led::m_bBlinkCountHasToggle = !mkii::Led::m_bBlinkCountHasToggle;
       // Reset interrupt
-      Top::GetTop()->GetTimer32()->SetCounter(Led::m_u32TimerCount);
-      Top::GetTop()->GetTimer32()->EnableInterruption(true);
+      Top::GetTop()->GetTimer32()->SetCounter(mkii::Led::m_u32TimerCount);
+      Top::GetTop()->GetTimer32()->EnableInterrupt(true);
       return;
     }
   } else {
     // Shouldn't be blinking then
     return;
   }
-}
-
 
 }
