782274f8f02e924ee8612e9dd249bd4117540a08
Emilio Rojas <emilio93@gmail.com>
2019-05-08 01:33:11 -0600

    Add Init function to mkii::LightSensor

--- include/mkii/LightSensor.hpp
+++ include/mkii/LightSensor.hpp
@@ -11,6 +11,7 @@ namespace mkii {
 class LightSensor {
  public:
 	LightSensor();
+	void Init();
 	float GetLux();
 
  private:
--- src/mkii/LightSensor.cpp
+++ src/mkii/LightSensor.cpp
@@ -4,6 +4,13 @@ mkii::LightSensor::LightSensor() {
 	this->m_pOPT3001 = new peripheral::OPT3001();
 }
 
+void mkii::LightSensor::Init() {
+	this->GetOPT3001()->GetI2C()->Init_GPIO();
+	this->GetOPT3001()->GetI2C()->Init();
+	this->GetOPT3001()->Init();
+	__delay_cycles(100000);
+}
+
 float mkii::LightSensor::GetLux() { return this->m_pOPT3001->GetLux(); }
 
 peripheral::OPT3001* mkii::LightSensor::GetOPT3001() {
