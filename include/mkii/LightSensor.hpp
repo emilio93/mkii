b9b920e01288440ad15bef5ef284fe89e468421a
Emilio Rojas <emilio93@gmail.com>
2019-05-08 01:33:30 -0600

    Add documentation to mkii::LightSensor

--- include/mkii/LightSensor.hpp
+++ include/mkii/LightSensor.hpp
@@ -8,14 +8,44 @@
 
 namespace mkii {
 
+/**
+ * A light sensor can read its measurement in lux units.
+ *
+ */
 class LightSensor {
  public:
+
+	/**
+	 * @brief Construct a new Light Sensor object initializing the OPT3001 object.
+	 *
+	 */
 	LightSensor();
+
+	/**
+	 * Sets up the i2c communication scheme.
+	 *
+	 */
 	void Init();
+
+	/**
+	 * Obtain the measurement in lux units.
+	 *
+	 * @return float Th measurement in lux units.
+	 */
 	float GetLux();
 
  private:
+	/**
+	 * The OPT3001 device used in the implementation.
+	 *
+	 */
 	peripheral::OPT3001* m_pOPT3001;
+
+	/**
+	 * Opbtain the OPT3001 device object.
+	 *
+	 * @return peripheral::OPT3001* The device object.
+	 */
 	peripheral::OPT3001* GetOPT3001();
 };
 
