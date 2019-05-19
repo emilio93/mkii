58687c0e7a971ad38aeafd51ba9313cfcac45b80
Emilio Rojas <emilio93@gmail.com>
2019-05-19 15:44:35 -0600

    Add documentation to Spi

--- include/peripheral/lcdScreen/Spi.hpp
+++ include/peripheral/lcdScreen/Spi.hpp
@@ -50,7 +50,21 @@ const uint32_t EUSCI_BASE = EUSCI_B0_BASE;
 
 class Spi {
  public:
+	/**
+	 * Construct the object with default values.
+	 *
+	 */
 	Spi();
+
+	/**
+	 * Constructs the object with given parameters.
+	 *
+	 * @param i_pSckGPIO GPIO for the Sck.
+	 * @param i_pMosiGPIO GPIO for the Mosi.
+	 * @param i_pRstGPIO GPIO for the Rst.
+	 * @param i_pCsGPIO GPIO for the Cs.
+	 * @param i_pDcGPIO GPIO for the Dc.
+	 */
 	Spi(peripheral::GPIO* i_pSckGPIO, peripheral::GPIO* i_pMosiGPIO,
 	    peripheral::gpio::OutputGPIO* i_pRstGPIO,
 	    peripheral::gpio::OutputGPIO* i_pCsGPIO,
@@ -61,23 +75,105 @@ class Spi {
 	void PortInit(void);
 	void SpiInit(void);
 
+	/**
+	 * @brief Set the SckGPIO object.
+	 *
+	 * @param i_pSckGPIO The SckGPIO object.
+	 */
 	void SetSckGPIO(peripheral::GPIO* i_pSckGPIO);
+
+	/**
+	 * @brief Set the MosiGPIO object.
+	 *
+	 * @param i_pMosiGPIO The MosiGPIO object.
+	 */
 	void SetMosiGPIO(peripheral::GPIO* i_pMosiGPIO);
+
+	/**
+	 * @brief Set the RstGPIO object.
+	 *
+	 * @param i_pRstGPIO The RstGPIO object.
+	 */
 	void SetRstGPIO(peripheral::gpio::OutputGPIO* i_pRstGPIO);
+
+	/**
+	 * @brief Set the CsGPIO object.
+	 *
+	 * @param i_pCsGPIO The CsGPIO object.
+	 */
 	void SetCsGPIO(peripheral::gpio::OutputGPIO* i_pCsGPIO);
+
+	/**
+	 * @brief Set the DcGPIO object.
+	 *
+	 * @param i_pDcGPIO The DcGPIO object.
+	 */
 	void SetDcGPIO(peripheral::gpio::OutputGPIO* i_pDcGPIO);
 
+	/**
+	 * @brief Get the SckGPIO object.
+	 *
+	 * @return peripheral::GPIO* The SckGPIO object.
+	 */
 	peripheral::GPIO* GetSckGPIO(void);
+
+	/**
+	 * @brief Get the MosiGPIO object.
+	 *
+	 * @return peripheral::GPIO* The MosiGPIO object.
+	 */
 	peripheral::GPIO* GetMosiGPIO(void);
+
+	/**
+	 * @brief Get the RstGPIO object.
+	 *
+	 * @return peripheral::gpio::OutputGPIO* The RstGPIO object.
+	 */
 	peripheral::gpio::OutputGPIO* GetRstGPIO(void);
+
+	/**
+	 * @brief Get the CsGPIO object.
+	 *
+	 * @return peripheral::gpio::OutputGPIO* The CsGPIO object.
+	 */
 	peripheral::gpio::OutputGPIO* GetCsGPIO(void);
+
+	/**
+	 * @brief Get the DcGPIO object.
+	 *
+	 * @return peripheral::gpio::OutputGPIO* The DcGPIO object.
+	 */
 	peripheral::gpio::OutputGPIO* GetDcGPIO(void);
 
  private:
+	/**
+	 * @brief GPIO for the Sck.
+	 *
+	 */
 	peripheral::GPIO* m_pSckGPIO;
+
+	/**
+	 * @brief GPIO for the Mosi.
+	 *
+	 */
 	peripheral::GPIO* m_pMosiGPIO;
+
+	/**
+	 * @brief GPIO for the Rst.
+	 *
+	 */
 	peripheral::gpio::OutputGPIO* m_pRstGPIO;
+
+	/**
+	 * @brief GPIO for the Cs.
+	 *
+	 */
 	peripheral::gpio::OutputGPIO* m_pCsGPIO;
+
+	/**
+	 * @brief GPIO for the Dc.
+	 *
+	 */
 	peripheral::gpio::OutputGPIO* m_pDcGPIO;
 };
 
